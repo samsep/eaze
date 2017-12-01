52d2bc5ff281a886975368718c333a86dcca2307
Sepasi <saminsepasi@gmail.com>
2017-12-01 15:13:45 -0800

    style updates for test.js

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-
 const test = require('tape');
 const series = require('run-series');
 const fs = require('fs');
@@ -6,54 +5,51 @@ const folderSize = require('get-folder-size');
 const download = require('./');
 const async = require('async');
 
-test('download', function (t) {
-  t.plan(3)
+test('download', (t) => {
+  t.plan(3);
 
   const COUNT = parseInt(process.env.COUNT, 10) || 10;
 
   series([
-    (callback) => download(COUNT, callback),
+    callback => download(COUNT, callback),
     verifyCount,
     verifySize,
-    verifyLodash
-  ], t.end)
+    verifyLodash,
+  ], t.end);
 
-  function verifyCount (callback) {
+  function verifyCount(callback) {
     fs.readdir('./packages', (err, files) => {
       if (err) return callback(err);
       // Filter .gitignore and other hidden files
       // Account for packages in same namespace such as @angular
-      let packageFiles = files.filter((file) => !/^\./.test(file) && !/^@/.test(file));
-      const nameSpaceDirectories = files.filter((file) => /^@/.test(file));
-      const asyncCb = (err) => {
-        if (err) return callback(err);
-        t.equal(packageFiles.length, COUNT, `has ${COUNT} files`)
+      let packageFiles = files.filter(file => !/^\./.test(file) && !/^@/.test(file));
+      const nameSpaceDirectories = files.filter(file => /^@/.test(file));
+      const asyncCb = (error) => {
+        if (error) return callback(error);
+        t.equal(packageFiles.length, COUNT, `has ${COUNT} files`);
         return callback();
       };
-      async.each(nameSpaceDirectories, (directory, cb) => {
-        fs.readdir(`./packages/${directory}`, (err, subFiles) => {
-          if (err) return cb(err);
+      return async.each(nameSpaceDirectories, (directory, cb) => {
+        fs.readdir(`./packages/${directory}`, (error, subFiles) => {
+          if (error) return cb(error);
           packageFiles = packageFiles.concat(subFiles);
-          cb();
-        })
+          return cb();
+        });
       }, asyncCb);
-      //count modules under same namespace, such as @angular
-      // t.equal(files.length, COUNT, `has ${COUNT} files`)
-      // callback()
-    })
+    });
   }
 
-  function verifySize (callback) {
-    folderSize('./packages', function (err, size) {
-      if (err) return callback(err)
-      t.ok(size / 1024 > 5 * COUNT, 'min 5k per package')
-      callback()
-    })
+  function verifySize(callback) {
+    folderSize('./packages', (err, size) => {
+      if (err) return callback(err);
+      t.ok(size / 1024 > 5 * COUNT, 'min 5k per package');
+      return callback();
+    });
   }
 
-  function verifyLodash (callback) {
-    const _ = require('./packages/lodash')
-    t.equal(typeof _.map, 'function', '_.map exists')
-    callback()
+  function verifyLodash(callback) {
+    const _ = require('./packages/lodash');
+    t.equal(typeof _.map, 'function', '_.map exists');
+    callback();
   }
-})
+});
