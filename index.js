6415c9bcd5483bafa0111068a884c812d6cd2c2c
Sepasi <saminsepasi@gmail.com>
2017-12-01 01:55:32 -0800

    add scraping functionality

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-'use strict'
+'use strict';
 
-module.exports = downloadPackages
+const getScrapedPackages = require('./lib/getScrapedPackages');
+const downloadFiles = require('./lib/downloadFiles');
+const winston = require('winston');
 
-function downloadPackages (count, callback) {
-
+function downloadPackages(count, callback) {
+  count = count || 10;
+  const directory = './packages';
+  getScrapedPackages(count, (err, packages) => {
+    if (err) return winston.error(err);
+    return downloadFiles(packages, directory, (error) => {
+      if (error) return winston.error(error);
+      return callback();
+    });
+  });
 }
+
+module.exports = downloadPackages();
